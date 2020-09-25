681d2d10f5f7b70ac98f14445b7bd7bc40a8cded
Alexis Cowles <alexis.cowles@patelhs.org>
2020-09-25 00:53:15 +0000

    added a working if statement

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int myAge = 15;
+    int myBirthday = 0110;
+    int todaysDate = 923;
+
+    if (todaysDate == 0110)
+    {
+    System.out.println("I am " +myAge+ " years old");
+    }else{
+    System.out.println("I am still " +myAge+ " years old");
+    }
+
   }
 }
