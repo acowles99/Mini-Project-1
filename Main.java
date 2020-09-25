532389e3c30df7fae5832b664efedf9a0b2cf142
Alexis Cowles <alexis.cowles@patelhs.org>
2020-09-25 16:00:19 +0000

    Finished the project

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
-class Main {
+class CheckMyAge {
   public static void main(String[] args) {
     int myAge = 15;
     int myBirthday = 0110;
     int todaysDate = 923;
 
-    if (todaysDate == 0110)
+    if (todaysDate == myBirthday)
     {
+      myAge= myAge +1;
     System.out.println("I am " +myAge+ " years old");
     }else{
     System.out.println("I am still " +myAge+ " years old");
